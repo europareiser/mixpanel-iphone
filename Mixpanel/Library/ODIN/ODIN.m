687f4af863e2182483388f96ea418007e9768a55
Wei Zhuo <weizhuo@gmail.com>
2013-05-20 15:01:04 +1000

    fix odin with ARC

--- Mixpanel/Library/ODIN/ODIN.m
+++ Mixpanel/Library/ODIN/ODIN.m
@@ -58,6 +58,7 @@ NSString * ODIN1(){
     
     if (sysctl(mib, 6, buf, &len, NULL, 0) < 0) {
         //NSLog(@"ODIN-1.1: sysctl 2 error");
+        free(buf);
         return nil;
     }
     
@@ -91,7 +92,7 @@ NSString * ODIN1(){
     
     free(buf);
     
-    NSString *odinstring = [[[NSString alloc] initWithString:(NSString*)string] autorelease];
+    NSString *odinstring = [[NSString alloc] initWithString:(__bridge NSString*)string];
     CFRelease(data);
     CFRelease(string);
 
